3b946c671e1d80e2215bf9cea0e46495dec7afdf
qwestandertty-pixel <qwestandertty@gmail.com>
2026-03-08 13:55:23 +0800

    Add files via upload

---
+++ search.ts
@@ -0,0 +1,12 @@
+{
+  "compilerOptions": {
+    "target": "ES2022",
+    "module": "ESNext",
+    "moduleResolution": "Bundler",
+    "strict": false,
+    "noImplicitAny": false,
+    "skipLibCheck": true,
+    "types": ["node"]
+  },
+  "include": ["api/**/*.ts", "shared/**/*.ts"]
+}
